9ea9820835073610d7c5267768f37d6275bcbd29
Ricardo Mantilla <mantillasanchezr@gmail.com>
2024-12-11 14:15:59 -0500

    validacion y generacion del pdf

--- src/main/java/com/example/demo/controllers/PdfController.java
+++ src/main/java/com/example/demo/controllers/PdfController.java
@@ -5,16 +5,18 @@ import com.example.demo.entities.SeguroIntegralCancer;
 import com.example.demo.repositories.SeguroIntegralCancerRepository;
 import com.example.demo.services.PdfService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.thymeleaf.context.Context;
 
+import java.io.ByteArrayInputStream;
+import java.nio.file.Path;
 import java.util.Base64;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -31,7 +33,8 @@ public class PdfController {
     }
 
     @GetMapping("/generate-pdf-base64-by-id")
-    public ResponseEntity<String> generatePdfBase64FromMongo(@RequestParam(value = "idSeguroIntegralCancer") final String idSeguroIntegralCancer) {
+    public ResponseEntity<InputStreamResource> generatePdfBase64FromMongo(@RequestParam(value = "idSeguroIntegralCancer") final String idSeguroIntegralCancer) {
+        final Long beginTime = System.nanoTime();
         try {
             final Context context = new Context();
 
@@ -39,42 +42,73 @@ public class PdfController {
             final SeguroIntegralCancer seguroIntegralCancer = optionalSeguroIntegralCancer.orElse(null);
 
             if (seguroIntegralCancer == null) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("El seguro no fue encontrado");
+                return ResponseEntity.internalServerError().build();
             }
+
+            final Long creacionPdf = System.nanoTime();
+            System.out.println("Base de datos: " + String.valueOf((creacionPdf - beginTime)/1000000));
 
             context.setVariable("seguro", seguroIntegralCancer);
 
+            // Generar el pdf a apartir de la plantilla
             final byte[] pdfBytes = pdfService.generatePdf("conditional-template", context);
+
+            System.out.println("Creacion pdf: " + String.valueOf((System.nanoTime() - creacionPdf)/1000000));
+
+            /*// Se pasa a base 64
             final String base64EncodedPdf = Base64.getEncoder().encodeToString(pdfBytes);
 
+            final Long creacionValidacion = System.nanoTime();
+            System.out.println("base 64: " + String.valueOf((base64 - creacionValidacion)/1000000));*/
+
+
+            // Creacion y validacion de la generacion del pdf
+            final Long initValidate = System.nanoTime();
+            //final Path pathPdf = pdfService.createAndValidatePdfFromBase64(base64EncodedPdf, "generated");
+            final Path pathPdf = pdfService.createAndValidatePdf(pdfBytes, "generated");
+
+            if(Objects.isNull(pathPdf)) {
+                return ResponseEntity.internalServerError().build();
+            }
+
+            pdfService.deleteFile(pathPdf.toString());
+
+            System.out.println("Validacion pdf: " + String.valueOf((System.nanoTime() - initValidate)/1000000));
+
+            final ByteArrayInputStream inputStream = new ByteArrayInputStream(pdfBytes);
+            final HttpHeaders headers = new HttpHeaders();
+
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=generated.pdf");
+
             return ResponseEntity.ok()
-                    .header(HttpHeaders.CONTENT_TYPE, "application/json")
-                    .body(base64EncodedPdf);
+                    .headers(headers)
+                    .contentType(MediaType.APPLICATION_PDF)
+                    .body(new InputStreamResource(inputStream));
 
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Error al generar el PDF: " + e.getMessage());
+            return ResponseEntity.internalServerError().build();
         }
     }
 
     @GetMapping("/generate-pdf-base64")
-    public ResponseEntity<String> generatePdfBase64(@RequestParam String tomador,
-                                                    @RequestParam String tipoDocumento,
-                                                    @RequestParam Long numeroDocumento,
-                                                    @RequestParam String fechaNacimiento,
-                                                    @RequestParam String genero,
-                                                    @RequestParam String nacionalidad,
-                                                    @RequestParam String pais,
-                                                    @RequestParam String email,
-                                                    @RequestParam Long celular,
-                                                    @RequestParam String direccion,
-                                                    @RequestParam String ciudad,
-                                                    @RequestParam Long numPoliza,
-                                                    @RequestParam Long precioSeguro,
-                                                    @RequestParam String fechaInicioVigencia,
-                                                    @RequestParam String fechaFinVigencia,
-                                                    @RequestParam Long cuantoCubreDiagnostico,
-                                                    @RequestParam Long cuantoCubreMuerte) {
+    public ResponseEntity<InputStreamResource> generatePdfBase64(@RequestParam String tomador,
+                                                                 @RequestParam String tipoDocumento,
+                                                                 @RequestParam Long numeroDocumento,
+                                                                 @RequestParam String fechaNacimiento,
+                                                                 @RequestParam String genero,
+                                                                 @RequestParam String nacionalidad,
+                                                                 @RequestParam String pais,
+                                                                 @RequestParam String email,
+                                                                 @RequestParam Long celular,
+                                                                 @RequestParam String direccion,
+                                                                 @RequestParam String ciudad,
+                                                                 @RequestParam Long numPoliza,
+                                                                 @RequestParam Long precioSeguro,
+                                                                 @RequestParam String fechaInicioVigencia,
+                                                                 @RequestParam String fechaFinVigencia,
+                                                                 @RequestParam Long cuantoCubreDiagnostico,
+                                                                 @RequestParam Long cuantoCubreMuerte) {
+        final Long beginTime = System.nanoTime();
         try {
             final Context context = new Context();
 
@@ -100,20 +134,39 @@ public class PdfController {
 
             context.setVariable("seguro", seguroDTO);
 
+            // Generar el pdf a apartir de la plantilla
             final byte[] pdfBytes = pdfService.generatePdf("conditional-template", context);
-            final String base64EncodedPdf = Base64.getEncoder().encodeToString(pdfBytes);
 
-            /*final HttpHeaders headers = new HttpHeaders();
-                headers.add("Content-Disposition", "attachment; filename=generated.pdf");
-                headers.add("Content-Type", "application/pdf");*/
+            final Long validacion = System.nanoTime();
+            System.out.println("Creacion pdf: " + String.valueOf((validacion - beginTime)/1000000));
+
+            // Se pasa a base 64
+            //final String base64EncodedPdf = Base64.getEncoder().encodeToString(pdfBytes);
+
+
+            // Creacion y validacion de la generacion del pdf
+            final Path pathPdf = pdfService.createAndValidatePdf(pdfBytes, "generated");
+
+            if(Objects.isNull(pathPdf)) {
+                return ResponseEntity.internalServerError().build();
+            }
+
+            pdfService.deleteFile(pathPdf.toString());
+
+            System.out.println("Validacion: " + String.valueOf((System.nanoTime() - validacion)/1000000));
+
+            final ByteArrayInputStream inputStream = new ByteArrayInputStream(pdfBytes);
+            final HttpHeaders headers = new HttpHeaders();
+
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=generated.pdf");
 
             return ResponseEntity.ok()
-                    .header(HttpHeaders.CONTENT_TYPE, "application/json")
-                    .body(base64EncodedPdf);
+                    .headers(headers)
+                    .contentType(MediaType.APPLICATION_PDF)
+                    .body(new InputStreamResource(inputStream));
 
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Error al generar el PDF: " + e.getMessage());
+            return ResponseEntity.internalServerError().build();
         }
     }
 }
--- src/main/java/com/example/demo/services/PdfService.java
+++ src/main/java/com/example/demo/services/PdfService.java
@@ -1,11 +1,18 @@
 package com.example.demo.services;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
 
 @Service
 public class PdfService {
@@ -33,5 +40,34 @@ public class PdfService {
         } catch (Exception e) {
             throw new RuntimeException("Error al generar el PDF", e);
         }
+    }
+
+    public Path createAndValidatePdf(final byte[] pdfBytes, final String fileName) throws IOException {
+        final String filePath = "";
+
+        if(!validatePdf(pdfBytes)){
+            return null;
+        }
+
+        final Path path = Files.write(Paths.get(filePath + fileName + ".pdf"), pdfBytes);
+
+        if (!Files.exists(path)) {
+            return null;
+        }
+
+        return path;
+    }
+
+    public boolean validatePdf(final byte[] pdfBytes) {
+        try {
+            PDDocument.load(pdfBytes).close();
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public boolean deleteFile(final String path) {
+        return new File(path).delete();
     }
 }
